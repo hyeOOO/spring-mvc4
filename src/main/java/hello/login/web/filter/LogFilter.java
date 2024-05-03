55b331d193885f8bdb026ded458f4a36804e4d57
hyeOOO <hyewon5342@nate.com>
2024-05-04 04:48:50 +0900

    로그인 처리 - 필터 사용

---
+++ src/main/java/hello/login/web/filter/LogFilter.java
@@ -0,0 +1,38 @@
+package hello.login.web.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+public class LogFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("log filter doFilter");
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+        try{
+            log.info("REQUEST [{}][{}]", uuid, requestURI);
+            chain.doFilter(request, response); //다음 필터가 있으면 다음 필터 호출, 없으면 서블릿 호출
+        }catch (Exception e){
+
+        }finally {
+            log.info("RESPONSE [{}][{}]", uuid, requestURI);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("log filter init");
+    }
+
+    @Override
+    public void destroy() {
+        log.info("log filter destroy");
+    }
+}
